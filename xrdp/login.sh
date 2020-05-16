e99053fd0299d7299fa15a1512c269abed1cce34
sbroers <44463486+sbroers@users.noreply.github.com>
2020-05-16 18:18:46 +0200

    Update login.sh

--- xrdp/login.sh
+++ xrdp/login.sh
@@ -3,17 +3,9 @@
 
 #cd ~/Downloads
 #cd $Dwnload
-wget https://github.com/sbroers/linux-on-hyper-v/blob/master/xrdp/griffon_logo_xrdp.bmp
-
-#Check if script has run once...
-if [ -f /etc/xrdp/xrdp.ini.griffon ]
-then
-sudo rm /etc/xrdp/xrdp.ini
-sudo mv /etc/xrdp/xrdp.ini.griffon /etc/xrdp/xrdp.ini
-fi 
-
-#Backup file 
-sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.griffon
+cd /tmp
+wget https://github.com/sbroers/linux-on-hyper-v/blob/master/xrdp/griffon_logo_xrdp.zip
+unzip griffon_logo_xrdp.zip
 
 #Check where to copy the logo file
 if [ -d "/usr/local/share/xrdp" ] 
@@ -27,12 +19,12 @@ else
 fi
 
 #4F194C
-sudo sed -i 's/blue=009cb5/blue=dedede/' /etc/xrdp/xrdp.ini
-sudo sed -i 's/#white=ffffff/white=dedede/' /etc/xrdp/xrdp.ini
-sudo sed -i 's/#ls_title=My Login Title/ls_title=Remote Desktop for Linux/' /etc/xrdp/xrdp.ini
+# sudo sed -i 's/blue=009cb5/blue=dedede/' /etc/xrdp/xrdp.ini
+# sudo sed -i 's/#white=ffffff/white=dedede/' /etc/xrdp/xrdp.ini
+sudo sed -i 's/#ls_title=My Login Title/ls_title=Enter User and Password/' /etc/xrdp/xrdp.ini
 
 # Background
-sudo sed -i 's/ls_top_window_bg_color=009cb5/ls_top_window_bg_color=4F194C/' /etc/xrdp/xrdp.ini
+# sudo sed -i 's/ls_top_window_bg_color=009cb5/ls_top_window_bg_color=4F194C/' /etc/xrdp/xrdp.ini
 
 sudo sed -i 's/ls_bg_color=dedede/ls_bg_color=ffffff/' /etc/xrdp/xrdp.ini
 sudo sed -i 's/ls_logo_x_pos=55/ls_logo_x_pos=0/' /etc/xrdp/xrdp.ini
