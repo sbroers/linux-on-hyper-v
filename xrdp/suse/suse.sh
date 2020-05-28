b3744d49fc817062c235ce53c14b9cb7ea278f68
sbroers <44463486+sbroers@users.noreply.github.com>
2020-05-28 11:36:12 +0200

    add suse

---
+++ xrdp/suse/suse.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+sudo zypper install xrdp
+
+sudo systemctl stop xrdp
+sudo systemctl stop xrdp-sesman
+
+# Configure the installed XRDP ini files.
+# use vsock transport.
+sed -i_orig -e 's/port=3389/port=vsock:\/\/-1:3389/g' /etc/xrdp/xrdp.ini
+# use rdp security.
+sed -i_orig -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
+# remove encryption validation.
+sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
+# disable bitmap compression since its local its much faster
+sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
+
+# use the script to setup the ubuntu session
+sed -i_orig -e 's/startwm/startubuntu/g' /etc/xrdp/sesman.ini
+
+# rename the redirected drives to 'shared-drives'
+sed -i -e 's/FuseMountName=thinclient_drives/FuseMountName=shared-drives/g' /etc/xrdp/sesman.ini
+
+# Changed the allowed_users
+sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
+
+# Blacklist the vmw module
+if [ ! -e /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf ]; then
+cat >> /etc/modprobe.d/blacklist_vmw_vsock_vmci_transport.conf <<EOF
+blacklist vmw_vsock_vmci_transport
+EOF
+fi
+
+#Ensure hv_sock gets loaded
+if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
+echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
+fi
+
+# Configure the policy xrdp session
+cat > /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla <<EOF
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+
+# reconfigure the service
+sudo systemctl enable xrdp
+sudo systemctl enable xrdp-sesmansudo systemctl daemon-reload
+sudo systemctl start xrdp
+
+
+
+echo "Install is complete."
+echo "Reboot your machine to begin using XRDP."
