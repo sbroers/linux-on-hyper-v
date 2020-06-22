ff9de166df137ea85b5ef10aec55fe3ade8362c6
sbroers <44463486+sbroers@users.noreply.github.com>
2020-06-22 10:25:06 +0200

    Update gui.sh

--- xrdp/gui switch/gui.sh
+++ xrdp/gui switch/gui.sh
@@ -9,10 +9,12 @@ while true
     echo "Wählen Sie die GUI aus, die Sie für xRDP verwenden möchten:"
     echo " "
     echo "[1] Gnome 3"
-    echo "[2] Xfce"
+    echo "[2] Gnome Ubuntu"
     echo "[3] KDE Plasma"
-    echo "[4] MATE"
-    echo "[5] Cinnamon"
+    echo "[4] KDE Kubuntu"
+    echo "[5] MATE"
+    echo "[6] Cinnamon"
+    echo "[7] Xfce"
     echo " "
     read -p "Eingabe:" gui
 
@@ -21,24 +23,31 @@ while true
         1) echo "gnome-session" > ~/.xsession
            GUI="Gnome 3"
            break ;;
-        2) echo "startxfce4" > ~/.xsession
-           GUI="Xfce"
+        2) echo "export GNOME_SHELL_SESSION_MODE=ubuntu" > ~/.xsession
+           echo "export XDG_CURRENT_DESKTOP=ubuntu:GNOME" >> ~/.xsession
+           echo "gnome-session" >> ~/x.session
+           GUI="Gnome-Ubuntu"      
            break ;;
         3) echo "startplasma-x11" > ~/.xsession
            GUI="KDE Plasma"
            break ;;
-        4) echo "mate-session" > ~/.xsession
+        4) echo "export XDG_DATA_DIRS=/usr/share/plasma:/usr/local/share:/usr/share:/var/lib/snapd/desktop" > ~/.xsession
+           echo "export XDG_CONFIG_DIRS=/etc/xdg/xdg-plasma:/etc/xdg:/usr/share/kubuntu-default-settings/kf5-settings" >> ~/.xsession
+           echo "export XDG_SESSION_DESKTOP=KDE"
+           echo "startplasma-x11" >> ~/.xsession
+           GUI="KDE Ubuntu"
+           break ;;
+        5) echo "mate-session" > ~/.xsession
            GUI="MATE"
            break ;;
-        5) echo "cinnamon" > ~/.xsession
+        6) echo "cinnamon" > ~/.xsession
            GUI="Cinnamon"
            break ;;
-        6) echo "export GNOME_SHELL_SESSION_MODE=ubuntu" > ~/x.session
-           echo "export XDG_CURRENT_DESKTOP=ubuntu:GNOME" >> ~/x.session
-           echo "gnome-session" >> ~/x.session 
+        7) echo "startxfce4" > ~/.xsession
+           GUI="Xfce"
            break ;;
         *) echo " "
-           echo "Geben Sie 1-5 ein! $gui ist eine ungültige Eingabe!"
+           echo "Geben Sie 1-6 ein! $gui ist eine ungültige Eingabe!"
            ;;
       esac
 done
