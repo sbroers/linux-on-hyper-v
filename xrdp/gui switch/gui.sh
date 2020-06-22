8c240bbe225e85ddefcca11d1adb0da00e0fed89
sbroers <44463486+sbroers@users.noreply.github.com>
2020-06-22 10:57:04 +0200

    Update gui.sh

--- xrdp/gui switch/gui.sh
+++ xrdp/gui switch/gui.sh
@@ -25,7 +25,7 @@ while true
            break ;;
         2) echo "export GNOME_SHELL_SESSION_MODE=ubuntu" > ~/.xsession
            echo "export XDG_CURRENT_DESKTOP=ubuntu:GNOME" >> ~/.xsession
-           echo "gnome-session" >> ~/x.session
+           echo "gnome-session" >> ~/.xsession
            GUI="Gnome-Ubuntu"      
            break ;;
         3) echo "startplasma-x11" > ~/.xsession
@@ -35,7 +35,7 @@ while true
            echo "export XDG_CONFIG_DIRS=/etc/xdg/xdg-plasma:/etc/xdg:/usr/share/kubuntu-default-settings/kf5-settings" >> ~/.xsession
            echo "export XDG_SESSION_DESKTOP=KDE"
            echo "startplasma-x11" >> ~/.xsession
-           GUI="KDE Ubuntu"
+           GUI="KDE Kubuntu"
            break ;;
         5) echo "mate-session" > ~/.xsession
            GUI="MATE"
@@ -46,8 +46,13 @@ while true
         7) echo "startxfce4" > ~/.xsession
            GUI="Xfce"
            break ;;
+        8) echo "export XDG_SESSION_DESKTOP=xubuntu" > ~/.xsession
+           echo "export XDG_DATA_DIRS=/usr/share/xfce4:/usr/share/xubuntu:/usr/local/share:/usr/share:/var/lib/snapd/desktop:/usr/share" >> ~/.xsession
+           echo "XDG_CONFIG_DIRS=/etc/xdg/xdg-xubuntu:/etc/xdg:/etc/xdg" >> ~/.xsession
+           GUI="Xfce Xubuntu"
+           break ;;
         *) echo " "
-           echo "Geben Sie 1-7 ein! $gui ist eine ungültige Eingabe!"
+           echo "Geben Sie 1-8 ein! $gui ist eine ungültige Eingabe!"
            ;;
       esac
 done
